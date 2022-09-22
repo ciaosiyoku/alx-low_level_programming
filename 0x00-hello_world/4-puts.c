134cd975f0bceb84a504ac812301be32589ee734
Adeh <adeh@Adehh.local>
2022-09-22 07:58:26 +0100

    C-Programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program printsa given statement
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
